6a819c691c445b5a179f683a73a22893ba9287a2
Ang-Wei-Liang <angweiliang19@gmail.com>
2023-09-20 18:20:55 +0800

    web 95.6 percent

--- app.py
+++ app.py
@@ -10,10 +10,28 @@ from datetime import datetime, timedelta
 
 #========================
 import firebase_admin
-from firebase_admin import credentials
+import os
+from firebase_admin import credentials, initialize_app
 
 # Initialize Firebase with your Firebase Admin SDK JSON file
-cred = credentials.Certificate('typebitsver2-firebase-adminsdk-m68lt-e6d733790b.json')
+# cred = credentials.Certificate('typebitsver2-firebase-adminsdk-m68lt-e6d733790b.json')
+
+# Initialize Firebase with environment variables
+firebase_credentials = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL"),
+}
+
+cred = credentials.Certificate(firebase_credentials)
+
 firebase_admin.initialize_app(cred)
 
 from firebase_admin import auth
